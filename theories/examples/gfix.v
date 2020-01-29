231a9e6554330b4d7452ecf80380aa605b1938fe
dcastrop <d.castro-perez@imperial.ac.uk>
2020-01-29 12:45:09 +0000

    Fixes

--- theories/examples/gfix.v
+++ theories/examples/gfix.v
@@ -1,49 +1,57 @@
-From mathcomp.ssreflect Require Import ssreflect ssrnat seq.
+From mathcomp.ssreflect Require Import all_ssreflect.
+
 Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
 
 Section FixGen.
 
-  CoInductive PCFix (S : Type) (P : Type -> Type) : Type :=
-  | GFix_fold (S' : Type) : P S' -> (S' -> S + PCFix S P) -> PCFix S P.
-  Definition GFix := PCFix False.
+  CoInductive GFix T (P : Type -> Type) : Type :=
+  | GFix_in : P T -> (T -> GFix T P) -> GFix T P.
 
-  Inductive GFin S P : S + PCFix S P -> Prop :=
-  | Fin_fold (S' : Type) (p : P S') (f : S' -> S + PCFix S P) :
-      (forall (s : S'), GFin (f s)) ->
-      GFin (inr (GFix_fold p f)).
-  Definition Fin P : GFix P -> Prop := fun x => GFin (inr x).
+  Definition monotone P := forall X Y, (X -> Y) -> P X -> P Y.
 
-  Inductive PIFix (S : Type) (P : Type -> Type) : Type :=
-  | LFix_fold (S' : Type) : P S' -> (S' -> S + PIFix S P) -> PIFix S P.
-  Definition LFix := PCFix False.
+  Definition enumerable P Y := forall X, P X -> P Y * seq (Y * X).
 
-  (* Fixpoint fromGFix S P (p : PCFix S P) (pf : GFin (inr p)) {struct pf} : PIFix S P *)
-  (*   := match p as p0 return p = p0 -> PIFix S P with *)
-  (*      | GFix_fold _ p f => _ *)
-  (*      end eq_refl. *)
-
-  Definition genF A B (f : A -> B) (x : A) : False + B := inr (f x).
-
-  Definition unF A B (f : A -> False + B) (x : A) : B :=
-    match f x with
-    | inl f => False_rect B f
-    | inr x => x
-    end.
-
-  Definition monotone P := forall {x y : Type}, (x -> y) -> P x -> P y.
-
-  Definition gfix_unfold P (map : monotone P) (x : GFix P)
-    : P (GFix P)%type :=
+  Definition gFix_out S P (map : monotone P) (x : GFix S P) : P (GFix S P) :=
     match x with
-    | GFix_fold _ p f => map _ (GFix P)%type (unF f) p
+    | GFix_in p f => map _ _ f p
     end.
+
+  CoFixpoint ana A (P : Type -> Type) (f : A -> P A) (x : A) : GFix A P :=
+    GFix_in (f x) (fun y => ana f y).
+
+  Inductive LFix S (P : Type -> Type) : Type :=
+  | LFix_in : P S -> (S -> LFix S P) -> LFix S P.
+
+  Definition lFix_out S P (map : monotone P) (x : LFix S P) : P (LFix S P) :=
+    match x with
+    | LFix_in p f => map _ _ f p
+    end.
+
+  Fixpoint cata A B P (map : monotone P) (f : P B -> B) (x : LFix A P) : B :=
+    match x with
+    | LFix_in p k => f (map _ _ (fun n => cata map f (k n)) p)
+    end.
+
+  Inductive Finite S P : GFix S P -> Prop :=
+  | Fin_fold (p : P S) (f : S -> GFix S P) :
+      (forall (s : S), Finite (f s)) -> Finite (GFix_in p f).
+
+  Lemma Finite_inv S P (p0 : GFix S P) (p : P S) (f : S -> GFix S P) :
+    Finite p0 -> p0 = GFix_in p f -> forall s, Finite (f s).
+  Proof. by case=> p1 f1 H1 [_ <-]. Defined.
+
+  Fixpoint fromGFix S P (p : GFix S P) (F : Finite p) {struct F} : LFix S P
+    := match p as p0 return p = p0 -> LFix S P with
+       | GFix_in p f =>
+         fun Pf => LFix_in p (fun n => fromGFix (Finite_inv F Pf n))
+       end erefl.
 
   Inductive treeP (A : Type) (t : Type) : Type :=
   | C : A -> list t -> treeP A t.
 
-  Definition citree A := GFix (treeP A).
+  Definition citree S A := GFix S (treeP A).
 
   Fixpoint downfrom n :=
     match n with
@@ -51,8 +59,5 @@ Section FixGen.
     | m.+1 => n :: downfrom m
     end.
 
-  CoFixpoint ana A (P : Type -> Type) (f : A -> P A) (x : A) : GFix P :=
-    GFix_fold (f x) (genF (fun y => ana f y)).
-
-  Definition exn (n : nat) : citree nat := ana (fun n => C n (downfrom n))n.
+  Definition exn (n : nat) : citree nat nat := ana (fun n => C n (downfrom n)) n.
 End FixGen.
